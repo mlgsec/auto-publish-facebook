1cb0b19018a63b7d5d8771f6bcc443ad29f788f1
Mateus Gomes <sec.mateuslgomes@gmail.com>
2024-07-15 19:36:13 -0300

    upload images

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from time import sleep
 
 import getpass
 
@@ -23,6 +24,31 @@ senha.send_keys(getpass.getpass("Password: "))
 entrar_button = driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[1]/form/div[2]/button')
 entrar_button.click()   
 
+
+input()
+
+driver.get("https://www.facebook.com/groups/434953423375127")
+
+images = [
+    "foto1.jpg",
+    "foto2.jpg",
+    "foto3.jpg",
+    "foto4.jpg",
+    "foto5.jpg",
+    "foto6.jpg",
+]
+
+value = 1
+
+for image in images:
+    try: 
+        sleep(0.5)
+        xpath_input = f'(//input[@type="file"])[{value}]'
+        input_arquivo = driver.find_element(By.XPATH, xpath_input)
+        input_arquivo.send_keys(image)
+        value = 3
+    except Exception as e:
+        print(f"Erro ao enviar arquivo: {e}")
 
 input()
 driver.quit()
